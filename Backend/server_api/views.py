86e540e27d362989eb499115195d9ebad56de2f6
Ved Nig <vednig12@outlook.com>
2022-08-29 03:26:00 +0530

    s

--- Backend/server_api/views.py
+++ Backend/server_api/views.py
@@ -4,8 +4,7 @@ import redis
 from django.http.response import JsonResponse
 
 def api(request,id):
-    r = redis.from_url('redis-18366.c305.ap-south-1-1.ec2.cloud.redislabs.com:18366')
-
+    r = redis.from_url('redis://redis-18366.c305.ap-south-1-1.ec2.cloud.redislabs.com:18366')
     r.ping()
     if request.method == 'GET': 
             now = datetime.datetime.now()
