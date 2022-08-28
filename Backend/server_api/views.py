287e9fe822aa028c341a8208b53f6da7970aeb3f
Ved Nig <vednig12@outlook.com>
2022-08-29 03:31:53 +0530

    str

--- Backend/server_api/views.py
+++ Backend/server_api/views.py
@@ -2,9 +2,10 @@ from django.http import HttpResponse
 import datetime
 import redis
 from django.http.response import JsonResponse
+import os
 
 def api(request,id):
-    r = redis.from_url('redis://redis-18366.c305.ap-south-1-1.ec2.cloud.redislabs.com:18366')
+    r = redis.Redis(host='redis-18366.c305.ap-south-1-1.ec2.cloud.redislabs.com', port=18366, username='default', password=str(os.environ['USER']), decode_responses=True)
     r.ping()
     if request.method == 'GET': 
             now = datetime.datetime.now()
