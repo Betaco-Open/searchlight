80efc27517cafdade3d22f3a374da6c71606bce4
Ved Nig <vednig12@outlook.com>
2022-08-29 03:23:33 +0530

    Redis

--- Backend/server_api/views.py
+++ Backend/server_api/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 import datetime
-
+import redis
 from django.http.response import JsonResponse
 
 def api(request,id):
-        if request.method == 'GET': 
+    r = redis.Redis()
+    r.ping()
+    if request.method == 'GET': 
             now = datetime.datetime.now()
             html = {"id":id}
             return JsonResponse(html)
