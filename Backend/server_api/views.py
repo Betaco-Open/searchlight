eb43170dc7dbb662a4ca79fcf05f0eec4eeab046
Ved Nig <vednig12@outlook.com>
2022-08-29 03:25:22 +0530

    credentials updated

--- Backend/server_api/views.py
+++ Backend/server_api/views.py
@@ -4,7 +4,8 @@ import redis
 from django.http.response import JsonResponse
 
 def api(request,id):
-    r = redis.Redis()
+    r = redis.from_url('redis-18366.c305.ap-south-1-1.ec2.cloud.redislabs.com:18366')
+
     r.ping()
     if request.method == 'GET': 
             now = datetime.datetime.now()
