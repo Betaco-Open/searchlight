844f633939cf2102aa1a02f65f3de904b4205745
Ved Nig <64083352+vednig@users.noreply.github.com>
2022-08-28 13:14:38 +0530

    Added Frontend with Flutter

---
+++ Frontend/new_proj_flutter/linux/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,31 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <bitsdojo_window_linux/bitsdojo_window_plugin.h>
+#include <flutter_acrylic/flutter_acrylic_plugin.h>
+#include <flutter_platform_alert/flutter_platform_alert_plugin.h>
+#include <flutter_window_close/flutter_window_close_plugin.h>
+#include <hotkey_manager/hotkey_manager_plugin.h>
+
+void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) bitsdojo_window_linux_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "BitsdojoWindowPlugin");
+  bitsdojo_window_plugin_register_with_registrar(bitsdojo_window_linux_registrar);
+  g_autoptr(FlPluginRegistrar) flutter_acrylic_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterAcrylicPlugin");
+  flutter_acrylic_plugin_register_with_registrar(flutter_acrylic_registrar);
+  g_autoptr(FlPluginRegistrar) flutter_platform_alert_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterPlatformAlertPlugin");
+  flutter_platform_alert_plugin_register_with_registrar(flutter_platform_alert_registrar);
+  g_autoptr(FlPluginRegistrar) flutter_window_close_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterWindowClosePlugin");
+  flutter_window_close_plugin_register_with_registrar(flutter_window_close_registrar);
+  g_autoptr(FlPluginRegistrar) hotkey_manager_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "HotkeyManagerPlugin");
+  hotkey_manager_plugin_register_with_registrar(hotkey_manager_registrar);
+}
---
+++ Frontend/new_proj_flutter/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,26 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <bitsdojo_window_windows/bitsdojo_window_plugin.h>
+#include <flutter_acrylic/flutter_acrylic_plugin.h>
+#include <flutter_platform_alert/flutter_platform_alert_plugin.h>
+#include <flutter_window_close/flutter_window_close_plugin.h>
+#include <hotkey_manager/hotkey_manager_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  BitsdojoWindowPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("BitsdojoWindowPlugin"));
+  FlutterAcrylicPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterAcrylicPlugin"));
+  FlutterPlatformAlertPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterPlatformAlertPlugin"));
+  FlutterWindowClosePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterWindowClosePlugin"));
+  HotkeyManagerPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("HotkeyManagerPlugin"));
+}
