9e67cda9409df5ae94158c28664f41190f52a863
Ved Nig <64083352+vednig@users.noreply.github.com>
2022-08-29 02:46:08 +0530

    Init

--- Backend/server_api/views.py
+++ Backend/server_api/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 import datetime
 
+from django.http.response import JsonResponse
+
 def current_datetime(request,id):
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % id
-    return HttpResponse(html)
+    return JsonResponse(html)
