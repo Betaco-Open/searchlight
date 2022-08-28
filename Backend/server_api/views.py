ea53047c00031a7eddf0cf72846350eab3b3656e
Ved Nig <vednig12@outlook.com>
2022-08-29 03:46:49 +0530

    minor changes

--- Backend/server_api/views.py
+++ Backend/server_api/views.py
@@ -9,7 +9,7 @@ def api(request,id):
     r = redis.Redis(host='redis-18366.c305.ap-south-1-1.ec2.cloud.redislabs.com', port=18366, username='default', password=str(os.environ['PASSWORD']), decode_responses=True)
 
     r.ping()    
-    client = pymongo.MongoClient("mongodb+srv://user12:{}@cluster0.iattu0o.mongodb.net/?retryWrites=true&w=majority".format(os.environ['MONGO_PASSWORD']), server_api=ServerApi('1'))
+    client = pymongo.MongoClient("mongodb+srv://user12:{}@cluster0.iattu0o.mongodb.net/?retryWrites=true&w=majority".format(os.environ['MONGO_PASSWORD']), )#server_api=ServerApi('1'))
     db = client.test
 
     if request.method == 'GET': 
