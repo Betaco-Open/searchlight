464a42f3f1fb4d3a31deb5d87ff005e4d673a54b
Ved Nig <vednig12@outlook.com>
2022-08-29 22:31:07 +0530

    minor

--- Backend/server_api/server_api/urls.py
+++ Backend/server_api/server_api/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from re import search
 from django.contrib import admin
 from django.urls import path
-from Backend.server_api.views import createGroup
 from views import *
 urlpatterns = [
     #path('admin/', admin.site.urls),
